05984cc05a0d8ea783da968076daf8256f64431b
Syedmha <syedabedi12@gmail.com>
2022-10-16 15:42:55 +0530

    Add: Step 3 UI

--- src/Components/FormMainContent/FormIndex.js
+++ src/Components/FormMainContent/FormIndex.js
@@ -2,13 +2,14 @@ import React from 'react'
 import FormStep1 from './FormStep1/FormStep1'
 import FormStep2 from './FormStep2/FormStep2'
 import '../FormMainContent/FormIndex.css'
+import FormStep3 from './FormStep3/FormStep3'
 
 function FormIndex() {
     return (
         <>
             <div className='form-content-container'>
                 {/* <FormStep1 /> */}
-                <FormStep2 />
+                <FormStep3 />
             </div>
         </>
     )
--- src/Components/FormMainContent/FormStep3/FormStep3.js
+++ src/Components/FormMainContent/FormStep3/FormStep3.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import '../FormStep3/FormStep3.css'
+import { Heading, Stack, CheckboxGroup, Checkbox, Box } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/react'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+
 
 function FormStep3() {
     return (
-        <div>FormStep3</div>
+        <>
+            <div className='step3-container'>
+                <div className='txt-container'>
+                    <IconButton marginRight={'1.5rem'} isRound size={'lg'} aria-label='Search database' icon={<ChevronLeftIcon />} />
+                    <Heading size='lg'>Declaration</Heading>
+                </div>
+                <div className='checkbox'>
+                    <CheckboxGroup colorScheme='green' defaultValue={['naruto', 'kakashi']}>
+                        <Stack spacing={[1, 5]} direction={['column']} >
+                            <Checkbox ><Text fontSize={'xl'} noOfLines={3}>
+                                I hereby declare that none of the proposed members are habitual consumers of alcohol, tobacco, gutka or any recreational drugs.</Text></Checkbox>
+                            <Checkbox ><Text fontSize={'xl'} noOfLines={3}>
+                                I hereby declare that all proposed members are in good health and entirely free from any mental pf physical impairements or deformities, disease/condition.</Text></Checkbox>
+                            <Checkbox ><Text fontSize={'xl'} noOfLines={3}>
+                                I have understood that there is 30 days waiting period for all diseases and 2 years on named ailments. (list of named ailements)</Text></Checkbox>
+                            <Checkbox ><Text fontSize={'xl'} noOfLines={3}>
+                                I understand that this policy doesn't cover Pre-existing diseases.</Text></Checkbox>
+                        </Stack>
+                    </CheckboxGroup>
+                </div>
+            </div>
+        </>
     )
 }
 
